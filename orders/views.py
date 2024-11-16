c4118003f3dac4f5f965eab8d741585cd9c1e2a6
Fenyxz <enmadofenyxz@gmail.com>
2024-11-16 14:48:25 +0000

    Implementing Stripe

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
@@ -22,12 +23,9 @@ def order_create(request):
                     quantity=item["quantity"],
                 )
             cart.clear()
-            order_created.delay(order.id)
-            return render(
-                request, 
-                "orders/order/created.html", 
-                {"order": order}
-            )
+            # order_created.delay(order.id)
+            request.session["order_id"] = order.id
+            return redirect(reverse("payment:process"))
     else:
         form = OrderCreateForm()
     return render(
